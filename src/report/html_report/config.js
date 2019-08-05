fc2dadd247449610164e740e4bb245185dcb8200
unknown <shklyarigor2015@gmail.com>
2019-08-05 17:46:47 +0300

    deploy static content

---
+++ src/report/html_report/config.js
@@ -0,0 +1,173 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\127925639_document_0_document_0_300.png",
+        "test": "..\\bitmaps_test\\20190805-174249\\127925639_document_0_document_0_300.png",
+        "selector": "document",
+        "fileName": "127925639_document_0_document_0_300.png",
+        "label": "document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/antisnake/",
+        "expect": 0,
+        "viewportLabel": "300",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\127925639_document_0_document_1_450.png",
+        "test": "..\\bitmaps_test\\20190805-174249\\127925639_document_0_document_1_450.png",
+        "selector": "document",
+        "fileName": "127925639_document_0_document_1_450.png",
+        "label": "document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/antisnake/",
+        "expect": 0,
+        "viewportLabel": "450",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\127925639_document_0_document_2_600.png",
+        "test": "..\\bitmaps_test\\20190805-174249\\127925639_document_0_document_2_600.png",
+        "selector": "document",
+        "fileName": "127925639_document_0_document_2_600.png",
+        "label": "document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/antisnake/",
+        "expect": 0,
+        "viewportLabel": "600",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\127925639_document_0_document_3_750.png",
+        "test": "..\\bitmaps_test\\20190805-174249\\127925639_document_0_document_3_750.png",
+        "selector": "document",
+        "fileName": "127925639_document_0_document_3_750.png",
+        "label": "document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/antisnake/",
+        "expect": 0,
+        "viewportLabel": "750",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\127925639_document_0_document_4_900.png",
+        "test": "..\\bitmaps_test\\20190805-174249\\127925639_document_0_document_4_900.png",
+        "selector": "document",
+        "fileName": "127925639_document_0_document_4_900.png",
+        "label": "document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/antisnake/",
+        "expect": 0,
+        "viewportLabel": "900",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\127925639_document_0_document_5_1050.png",
+        "test": "..\\bitmaps_test\\20190805-174249\\127925639_document_0_document_5_1050.png",
+        "selector": "document",
+        "fileName": "127925639_document_0_document_5_1050.png",
+        "label": "document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/antisnake/",
+        "expect": 0,
+        "viewportLabel": "1050",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\127925639_document_0_document_6_1200.png",
+        "test": "..\\bitmaps_test\\20190805-174249\\127925639_document_0_document_6_1200.png",
+        "selector": "document",
+        "fileName": "127925639_document_0_document_6_1200.png",
+        "label": "document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/antisnake/",
+        "expect": 0,
+        "viewportLabel": "1200",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
